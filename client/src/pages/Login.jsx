593a4a00fada7708c369b1c45a14f2c1566c0131
bvsaimanvith <bvsaimanvith@gmail.com>
2023-12-09 23:47:56 +0530

    updated

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,22 +1,40 @@
+// JSX with classNames
 import { useState } from "react";
+import './Login.css';
 
 export default function Login() {
-  const[data,setData]=useState({
-    email:'',
-    pswd:'',
-})
-  const loginUser=(e)=>{
+  const [data, setData] = useState({
+    email: '',
+    pswd: '',
+  });
+
+  const loginUser = (e) => {
     e.preventDefault();
-  }
+  };
+
   return (
-    <div>
-      <form onSubmit={loginUser}>
-        <label>Email</label>
-        <input type='email' placeholder="enter email..." value={data.email} onChange={(e)=>setData({...data, email:e.target.value})}/>
-        <label>Password</label>
-        <input type='password' placeholder="enter password.... " value={data.pswd} onChange={(e)=>setData({...data, pswd:e.target.value})}/>
-        <button type="submit">Login</button>
-    </form>
+    <div className="login-container">
+      <form className="login-form" onSubmit={loginUser}>
+        <label className="login-label">Email</label>
+        <input
+          className="login-input"
+          type="email"
+          placeholder="Enter email..."
+          value={data.email}
+          onChange={(e) => setData({ ...data, email: e.target.value })}
+        />
+        <label className="login-label">Password</label>
+        <input
+          className="login-input"
+          type="password"
+          placeholder="Enter password.... "
+          value={data.pswd}
+          onChange={(e) => setData({ ...data, pswd: e.target.value })}
+        />
+        <button className="login-button" type="submit">
+          Login
+        </button>
+      </form>
     </div>
-  )
+  );
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,25 +1,47 @@
+// JSX with classNames
 import { useState } from "react";
+import './register.css';
 
 export default function Register() {
-    const[data,setData]=useState({
-        name:'',
-        email:'',
-        pswd:'',
-    })
-    const registerUser=(e)=>{
+    const [data, setData] = useState({
+        name: '',
+        email: '',
+        pswd: '',
+    });
+
+    const registerUser = (e) => {
         e.preventDefault();
-    }
-  return (
-    <div>
-    <form onSubmit={registerUser}>
-        <label>Name</label>
-        <input type='text' placeholder="enter name...." value={data.name} onChange={(e)=>setData({...data, name:e.target.value})}/>
-        <label>Email</label>
-        <input type='email' placeholder="enter email..." value={data.email} onChange={(e)=>setData({...data, email:e.target.value})}/>
-        <label>Password</label>
-        <input type='password' placeholder="enter password.... " value={data.pswd} onChange={(e)=>setData({...data, pswd:e.target.value})}/>
-        <button type="submit">Submit</button>
-    </form>
-    </div>
-  )
+    };
+
+    return (
+        <div className="register-container">
+            <form className="register-form" onSubmit={registerUser}>
+                <label className="register-label">Name</label>
+                <input
+                    className="register-input"
+                    type='text'
+                    placeholder="Enter name...."
+                    value={data.name}
+                    onChange={(e) => setData({ ...data, name: e.target.value })}
+                />
+                <label className="register-label">Email</label>
+                <input
+                    className="register-input"
+                    type='email'
+                    placeholder="Enter email..."
+                    value={data.email}
+                    onChange={(e) => setData({ ...data, email: e.target.value })}
+                />
+                <label className="register-label">Password</label>
+                <input
+                    className="register-input"
+                    type='password'
+                    placeholder="Enter password.... "
+                    value={data.pswd}
+                    onChange={(e) => setData({ ...data, pswd: e.target.value })}
+                />
+                <button className="register-button" type="submit">Submit</button>
+            </form>
+        </div>
+    );
 }
