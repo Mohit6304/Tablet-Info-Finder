9f9087b95c837cef4f175ec0bc2dc625e812cd94
bvsaimanvith <bvsaimanvith@gmail.com>
2023-12-10 00:57:18 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,10 +9,29 @@ import About from './pages/About';
 import Footer from './components/Footer';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import FOG from 'vanta/src/vanta.fog';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(()=>{
+    FOG({
+      el:'#vanta',
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      highlightColor: 0xeddab1,
+      midtoneColor: 0xffffff,
+      lowlightColor: 0x382e93,
+      baseColor: 0xffffff,
+      speed: 1.50
+    })
+  },[])
   return (
     <>
+    <div className='bg' id='vanta'>
+      <div className='in'>
       <Navbar />
       <Routes>
           <Route exact path="/" element={<Home />} />
@@ -24,6 +43,8 @@ function App() {
           <Route path="/register" element={<Register/>}/>
       </Routes>
       <Footer/>
+      </div>
+      </div>
     </>
   );
 }
