af5c8b3d889b5f9e44a0c3040cf74e7740725ea9
bvsaimanvith <bvsaimanvith@gmail.com>
2023-12-10 23:18:52 +0530

    updated frontend

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,11 +1,87 @@
-import React from 'react'
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import styled from "styled-components";
+import { toast } from 'react-hot-toast';
+import { useNavigate } from "react-router-dom";
 
-function Contact() {
+const StyledContactForm = styled.div`
+  max-width: 400px;
+  margin: 0 auto;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+
+    label {
+      font-size: 14px;
+    }
+
+    input,
+    textarea {
+      padding: 10px;
+      font-size: 16px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+    }
+
+    input[type="submit"] {
+      background-color: #003c78;
+      color: #fff;
+      cursor: pointer;
+      border: none;
+      border-radius: 5px;
+      padding: 10px;
+      font-size: 16px;
+      transition: background-color 0.3s;
+    }
+
+    input[type="submit"]:hover {
+      background-color: #072e55;
+    }
+  }
+`;
+
+const Contact = () => {
+  const form = useRef();
+  const navigate=useNavigate();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_dqnbljm",
+        "template_39l6dho",
+        form.current,
+        // "rM_DWg96oPkdNql7m"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
+          toast.success("Message sent");
+          navigate('/');
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
-    <div>
-      Contact
-    </div>
-  )
-}
+    <StyledContactForm>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
+    </StyledContactForm>
+  );
+};
 
-export default Contact
+export default Contact;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,25 @@
-import {useContext} from 'react'
-import { UserContext } from '../../context/userContext';
+// import {useContext} from 'react'
+// import { UserContext } from '../../context/userContext';
+import './home.css'
+import logoImage from '../components/img/log.png';
+import { NavLink } from 'react-router-dom';
 
 export default function Home() {
-    const {user}=useContext(UserContext);
+    // const {user}=useContext(UserContext);
     return (
         <div>
-            {!!user && (<h2>hi {user.name} !!</h2>)}
+            {/* {!!user && (<h2>hi {user.name} !!</h2>)} */}
+            <div className="hero-section">
+            <div className="hero-content">
+                <h1>Discover Your Perfect Tablet</h1>
+                <p>Explore a wide range of tablets and find the one that suits your needs.</p>
+                <p><NavLink to="/tab"><button>Browse</button></NavLink></p>
+            </div>
+            <div className="hero-image">
+                <img src={logoImage} alt="Tablet Image" />
+            </div>
+            </div>
+
         </div>
     )
 }
